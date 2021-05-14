a3749ffc0f8b6c8f16f9ca0361316a2e89c20161
Kieran <belli.kieran@gmail.com>
2021-05-14 11:24:48 +0200

    page inscription, connexion, JS, manque encore de CSS

---
+++ js/Formulaire.js
@@ -0,0 +1,85 @@
+const inpNom = document.querySelector("#Nom");
+const inpMail = document.querySelector("#email");
+const inpMdp = document.querySelector("#mdp");
+const inpConfirme = document.querySelector("#mdpconf");
+
+const buttonConfirm = document.querySelector("#confirm");
+
+let check = {
+  user: false,
+  mdp: false,
+  mail: false
+};
+buttonConfirm.addEventListener("click", validation);
+
+const regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+
+function validation(event) {
+  //Si le champ est vide
+  if (regexNom.test(inpNom.value) == false) {
+    event.preventDefault();
+  }
+}
+
+inpMail.addEventListener("input", function(e) {
+  const regexEmail = /\S+@\S+\.\S+/;
+
+  console.log(e.target.value);
+  console.log(e.target.value.search(regexEmail));
+  if (e.target.value.search(regexEmail) === 0) {
+    check.mail = true;
+  } else if (e.target.value.search(regexEmail) === -1) {
+    check.mail = false;
+  }
+});
+
+// Validation création du MDP
+
+let valeurInp;
+const specialCar = /[^a-zA-Z0-9]/;
+const alphabet = /[a-z]/i;
+const chiffres = /[0-9]/;
+
+let objValidation = {
+  symbole: 0,
+  lettre: 0,
+  chiffre: 0
+};
+
+inpMdp.addEventListener("input", function(e) {
+  valeurInp = e.target.value;
+
+  if (valeurInp.search(specialCar) !== -1) {
+    objValidation.symbole = 1;
+  }
+  if (valeurInp.search(alphabet) !== -1) {
+    objValidation.lettre = 1;
+  }
+  if (valeurInp.search(chiffres) !== -1) {
+    objValidation.chiffre = 1;
+  }
+
+  if ((e.inputType = "deleteContentBackward")) {
+    if (valeurInp.search(specialCar) === -1) {
+      objValidation.symbole = 0;
+    }
+    if (valeurInp.search(alphabet) === -1) {
+      objValidation.lettre = 0;
+    }
+    if (valeurInp.search(chiffres) === -1) {
+      objValidation.chiffre = 0;
+    }
+  }
+
+  let testAll = 0;
+  for (const property in objValidation) {
+    if (objValidation[property] > 0) {
+      testAll++;
+    }
+  }
+  if (testAll < 3) {
+    check.mdp = false;
+  } else {
+    check.mdp = true;
+  }
+});
