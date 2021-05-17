790e0eff69a308119252f93269af7e7d42cfb67f
Loic-Gouriou <loicgouriou6@gmail.com>
2021-05-17 15:19:25 +0200

    connexion a la base

--- js/ClassementBières.js
+++ js/ClassementBières.js
@@ -17,3 +17,22 @@ $(function(){
 		$("#progress").css("width",barre);
 	});
 });
+
+
+function loadDoc() {
+	
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "http://localhost:3000/beer/search?search=['letters']";
+
+xhr.open(method, url, true);
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+	console.log('test');
+    console.log(xhr.responseText);
+  }
+};
+xhr.send();
+
+
+}
