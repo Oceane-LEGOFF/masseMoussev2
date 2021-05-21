04c9405584f688c7503120cb38d3b0ae64f7210c
Loic-Gouriou <loicgouriou6@gmail.com>
2021-05-21 09:43:43 +0200

    Connexion à la base faites

--- js/Formulaire.js
+++ js/Formulaire.js
@@ -6,31 +6,31 @@ const inpConfirme = document.querySelector("#mdpconf");
 const buttonConfirm = document.querySelector("#confirm");
 
 let check = {
-  user: false,
-  mdp: false,
-  mail: false
+user: false,
+mdp: false,
+mail: false
 };
 buttonConfirm.addEventListener("click", validation);
 
 const regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 
 function validation(event) {
-  //Si le champ est vide
-  if (regexNom.test(inpNom.value) == false) {
-    event.preventDefault();
-  }
+//Si le champ est vide
+if (regexNom.test(inpNom.value) == false) {
+  event.preventDefault();
+}
 }
 
 inpMail.addEventListener("input", function(e) {
-  const regexEmail = /\S+@\S+\.\S+/;
+const regexEmail = /\S+@\S+\.\S+/;
 
-  console.log(e.target.value);
-  console.log(e.target.value.search(regexEmail));
-  if (e.target.value.search(regexEmail) === 0) {
-    check.mail = true;
-  } else if (e.target.value.search(regexEmail) === -1) {
-    check.mail = false;
-  }
+console.log(e.target.value);
+console.log(e.target.value.search(regexEmail));
+if (e.target.value.search(regexEmail) === 0) {
+  check.mail = true;
+} else if (e.target.value.search(regexEmail) === -1) {
+  check.mail = false;
+}
 });
 
 // Validation création du MDP
@@ -41,89 +41,118 @@ const alphabet = /[a-z]/i;
 const chiffres = /[0-9]/;
 
 let objValidation = {
-  symbole: 0,
-  lettre: 0,
-  chiffre: 0
+symbole: 0,
+lettre: 0,
+chiffre: 0
 };
 
 inpMdp.addEventListener("input", function(e) {
-  valeurInp = e.target.value;
+valeurInp = e.target.value;
 
-  if (valeurInp.search(specialCar) !== -1) {
-    objValidation.symbole = 1;
-  }
-  if (valeurInp.search(alphabet) !== -1) {
-    objValidation.lettre = 1;
-  }
-  if (valeurInp.search(chiffres) !== -1) {
-    objValidation.chiffre = 1;
-  }
+if (valeurInp.search(specialCar) !== -1) {
+  objValidation.symbole = 1;
+}
+if (valeurInp.search(alphabet) !== -1) {
+  objValidation.lettre = 1;
+}
+if (valeurInp.search(chiffres) !== -1) {
+  objValidation.chiffre = 1;
+}
 
-  if ((e.inputType = "deleteContentBackward")) {
-    if (valeurInp.search(specialCar) === -1) {
-      objValidation.symbole = 0;
-    }
-    if (valeurInp.search(alphabet) === -1) {
-      objValidation.lettre = 0;
-    }
-    if (valeurInp.search(chiffres) === -1) {
-      objValidation.chiffre = 0;
-    }
+if ((e.inputType = "deleteContentBackward")) {
+  if (valeurInp.search(specialCar) === -1) {
+    objValidation.symbole = 0;
   }
+  if (valeurInp.search(alphabet) === -1) {
+    objValidation.lettre = 0;
+  }
+  if (valeurInp.search(chiffres) === -1) {
+    objValidation.chiffre = 0;
+  }
+}
 
-  let testAll = 0;
-  for (const property in objValidation) {
-    if (objValidation[property] > 0) {
-      testAll++;
-    }
+let testAll = 0;
+for (const property in objValidation) {
+  if (objValidation[property] > 0) {
+    testAll++;
   }
-  if (testAll < 3) {
-    check.mdp = false;
-  } else {
-    check.mdp = true;
-  }
+}
+if (testAll < 3) {
+  check.mdp = false;
+} else {
+  check.mdp = true;
+}
 });
 
 
-function VerifCompte() {
-	
+function test(){ 
   var email = document.getElementById("email").value;
-  var xhr = new XMLHttpRequest(),
-      method = "GET",
-      URL = 'http://localhost:3000/clients/searchM=search' + email;
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    URL = 'http://localhost:3000/clients/searchM?search=' + email;
+   var result = xhr.open(method, URL, true);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        alert("ca fonctionne")
+      }
+      else {
+        alert("Compte inexistant");
+      }
+    };
+    xhr.send();
+    }
+
+function VerifCompte() {
+
+var email = document.getElementById("email").value;
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    URL = 'http://localhost:3000/clients/searchM=search' + email;
 
 
-  var xhr2 = new XMLHttpRequest(),
-      method = "POST"
-      URL = 'http://localhost:3000/clients/";
-  
-  xhr.open(method, url, true);
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200 && xhr2 === xhr) {
-      alert("ca fonctionne")
-    }
-    else if (email != getClients.email ) {
-      alert("Compte inexistant");
-    }
-  };
-  xhr.send();
+// var xhr2 = new XMLHttpRequest(),
+//     method = "POST"
+//     URL = 'http://localhost:3000/clients/";
+
+xhr.open(method, URL, true);
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    alert("ca fonctionne")
   }
+  else if (email != getClients.email ) {
+    alert("Compte inexistant");
+  }
+};
+xhr.send();
+}
 
 
 function creationClient() {
-  var xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://localhost:3000/clients/', true);
+  var nom = document.getElementById("nom").value;
+  var email = document.getElementById("email").value;
+  var date = document.getElementById("bday").value;
+  var prenom = document.getElementById("Prénom").value;
+  var mdp = document.getElementById("mdpconf").value;
 
+
+
+console.log("création");
+  var xhr = new XMLHttpRequest(),
+      method = "POST",
+      URL = 'http://localhost:3000/clients';
+xhr.open(method, URL, true);
+
+console.log("créé");
 //Envoie les informations du header adaptées avec la requête
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+console.log("envoyéBis");
 
 xhr.onreadystatechange = function() { //Appelle une fonction au changement d'état.
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         console.log("envoyé");
     }
 }
-xhr.send("foo=bar&lorem=ipsum");
-// xhr.send(new Int8Array());
-// xhr.send(document);
+xhr.send(JSON.stringify({"name": nom, "prenom": prenom, "naissance" : date, "mail" : email, "mdp" : mdp}));
 
 }
